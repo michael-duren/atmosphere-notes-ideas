d5bc0ee9d9f7291810876f54fc56d9966e270631
Michael Duren <michaeld@michaelduren.com>
2023-06-23 08:32:56 -0500

    added rest of drums to store

--- atmosphere-ui-one/src/store/pattern/pattern.ts
+++ atmosphere-ui-one/src/store/pattern/pattern.ts
@@ -14,10 +14,28 @@ export interface PatternAction {
 
 export interface KitState {
   BD: DrumState;
+  SD: DrumState;
+  CL: DrumState;
+  CH: DrumState;
 }
 
 const patternInitialState: KitState = {
   // 16 boolean steps in an array
+  CH: {
+    steps: Array.from({ length: 16 }).map((val) => (val = false)),
+    name: 'CH',
+    sound: '/sounds/closed-hat.wav',
+  },
+  CL: {
+    steps: Array.from({ length: 16 }).map((val) => (val = false)),
+    name: 'CL',
+    sound: '/sounds/clap.wav',
+  },
+  SD: {
+    steps: Array.from({ length: 16 }).map((val) => (val = false)),
+    name: 'SD',
+    sound: '/sounds/snare.wav',
+  },
   BD: {
     steps: Array.from({ length: 16 }).map((val) => (val = false)),
     name: 'BD',
@@ -35,8 +53,6 @@ const kitSlice = createSlice({
         !state[action.payload.name as keyof KitState].steps[
           action.payload.step
         ];
-
-      console.log(state.BD.steps);
     },
   },
 });
